3cf22f6ad91c1ad466ff66d6f1f9e41e41342ee5
liwenhan220 <47604011+liwenhan220@users.noreply.github.com>
2023-12-05 20:11:09 -0500

    Add files via upload

--- extract_feature.py
+++ extract_feature.py
@@ -3,217 +3,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import List
 from tqdm import tqdm
-import math
+from frontier import Frontier
+from my_threshold import custom_adaptive_threshold
+from bacteria import Bacteria
 import pdb
-
-# Included to bound each bateria by a box
-class Bounds:
-    def __init__(self):
-        self.init = False
-        self.left = -1
-        self.right = -1
-        self.top = -1
-        self.bottom = -1
-
-    def add(self, coord):
-        # coord in form (x, y)
-        x, y = coord
-
-        # Init part
-        if not self.init:
-            self.left = y
-            self.right = y
-            self.top = x
-            self.bottom = x
-            self.init = True
-        
-        # Update boundaries
-        else:
-            self.left = min(self.left, y)
-            self.right = max(self.right, y)
-            self.top = min(self.top, x)
-            self.bottom = max(self.bottom, x)
-    
-    # shape of the bound
-    def suggest_shape(self):
-        return (self.bottom - self.top + 1, self.right - self.left + 1)
-
-    # return informations needed for the bound
-    def info(self):
-        return (self.left, self.right, self.top, self.bottom)
-
-    # Convert to string (easier to print)
-    def __str__(self):
-        return "left: {} right: {} top: {} bottom: {}".format(self.left, self.right, self.top, self.bottom)
-
-# Bateria object
-class Bacteria:
-    def __init__(self):
-        self.coords = [] # coordinates of the bacteria
-        self.x_coords = [] # X values of the coordinate
-        self.y_coords = [] # y values of the coordinate
-        self.img = None # img to store bacteria
-        self.bounds = Bounds() # box to bound bateria
-        self.label = None 
-        self.part_img = None
-        self.bg_mean = None
-        self.part_img_shape = (28, 28)
-
-    def add_coord(self, x, y):
-        # add to my lists
-        self.coords.append([x, y])
-        self.x_coords.append(x)
-        self.y_coords.append(y)
-
-        # update bounds
-        self.bounds.add((x, y))
-
-    def get_pixel_mean(self):
-        return self.pixel_mean
-
-    def retrieve_pixels(self, img):
-        self.img = np.zeros((*(self.bounds.suggest_shape()), 3)).astype(np.uint8)
-        self.pixels = []
-
-        for x, y in self.coords:
-            # Copy pixel
-            self.img[x-self.bounds.top][y-self.bounds.left][0] = img[x][y][0]
-            self.img[x-self.bounds.top][y-self.bounds.left][1] = img[x][y][1]
-            self.img[x-self.bounds.top][y-self.bounds.left][2] = img[x][y][2]
-    
-    def pad_img(self, desired_shape):
-        height, width = self.img_shape()
-        desired_height, desired_width = desired_shape
-        if desired_width < width or desired_height < height:
-            print("Cannot perform padding with desired shape smaller or equal to {}".format(self.img_shape()))
-            return -1
-        hdiff, wdiff = abs(desired_height - height), abs(desired_width - width)
-        self.img = cv2.copyMakeBorder(self.img, top = hdiff // 2, bottom = (hdiff + 1) // 2, left = wdiff // 2, right= (wdiff + 1)//2, borderType=cv2.BORDER_CONSTANT, value=0)
-        return 1
-
-    # def get_partial_img(self, img:np.ndarray):
-    #     l, r, t, b = self.bounds.info()
-    #     self.part_img = img[t:b+1, l:r+1]
-    #     return self.part_img
-    
-    def get_partial_img(self, img:np.ndarray):
-        xc, yc = self.get_center()
-        x_size, y_size = self.part_img_shape
-        t = max(xc - int(x_size // 2), 0)
-        l = max(yc - int(y_size // 2), 0)
-        b = min(xc + math.ceil(x_size / 2), len(img))
-        r = min(yc + math.ceil(y_size / 2), len(img[0]))
-        self.part_img = img[t:b+1, l:r+1]
-        return self.part_img
-    
-    def bg_img(self, bias = 3):
-        pimg = cv2.cvtColor(self.part_img, cv2.COLOR_BGR2GRAY)
-
-        _, pimg = cv2.threshold(pimg, np.mean(pimg) - bias, 255, cv2.THRESH_BINARY)
-        mask = (pimg/255).astype(np.uint8)
-        bg_img = cv2.bitwise_and(self.part_img, self.part_img, mask=mask)
-        return bg_img
-    
-    def get_bg_mean(self):
-        if self.bg_mean is None:
-            b_img = self.bg_img()
-
-            pixel_sum = b_img.sum(0).sum(0)
-            count = (b_img!=0).sum(0).sum(0)
-            self.bg_mean = pixel_sum / count
-        return self.bg_mean
-    
-    def bg_normalized(self):
-        return ((self.img / self.get_bg_mean())).astype(np.float32)
-    
-    def img_mean(self, img):
-        g_img = np.mean(img, axis = 2)
-        f_img = g_img.flatten()
-        return f_img.sum() / (f_img != 0).sum()
-        
-    def imshow(self):
-        plt.imshow(self.img)
-        plt.show()
-
-    def size(self):
-        return len(self.x_coords)
-    
-    def img_shape(self):
-        if self.img is not None:
-            return (self.img.shape[0], self.img.shape[1])
-        else:
-            return self.bounds.suggest_shape()
-    
-    def get_feature(self):
-        return self.img
-
-    def is_boundary(self, x, y):
-        for i, j in [[1,0], [0,1], [1,1], [1,-1]]:
-            for a in [1, -1]:
-                xx = x + i * a
-                yy = y + j * a
-                if [xx, yy] not in self.coords:
-                    return True
-        return False
-    
-    def dist(self, pt1, pt2):
-        return math.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2)
-    
-    def get_end_pts(self):
-        max_d = 0
-        init_pt = self.coords[0]
-        max_pt = init_pt
-        for pt in self.coords:
-            dist = self.dist(pt, init_pt)
-            if dist > max_d:
-                max_pt = pt
-                max_d = dist
-        
-        max_d = 0
-        init_pt = max_pt
-        for pt in self.coords:
-            dist = self.dist(pt, init_pt)
-            if dist > max_d:
-                max_pt = pt
-                max_d = dist
-        return init_pt, max_pt
-    
-    def get_center(self):
-        pt1, pt2 = self.get_end_pts()
-        return [int((pt1[0] + pt2[0])/2), int((pt1[1] + pt2[1])/2)]
-    
-    def est_diameter(self):
-        return self.dist(*self.get_end_pts())
-
-
-
-#####################
-# Frontier object
-class Frontier:
-    def __init__(self):
-        self.record = {}
-        self.frontier = []
-
-    def add(self, item):
-        self.frontier.append(item)
-        self.record[str(item)] = True
-
-    def pop(self):
-        item = self.frontier[0]
-        self.record[str(item)] = False
-        self.frontier = self.frontier[1:]
-        return item
-    
-    def contains(self, item):
-        key = str(item)
-        return (key in self.record) and self.record[key]
-
-    def isEmpty(self):
-        return len(self.frontier) == 0
-    
-
+import os
 #####################
 # Utilities 
+
+# get 8 symmetries of an image
+def get_symmetries(img):
+    symmetries = []
+    for i in range(4):
+        y = np.rot90(img, i)
+        for flip in [False, True]:
+            if flip:
+                y = np.fliplr(y)
+
+            symmetries.append(y)
+    return symmetries
+
+def get_feature_img(arr_of_imgs, arr_of_backgrounds):
+    results = []
+    for i in range(len(arr_of_imgs)):
+        img = arr_of_imgs[i]
+        bg = arr_of_backgrounds[i]
+        # results.append(img - (img != 0) * bg)
+        results.append(img / bg)
+    return np.array(results)
     
 # largest box to include both shapes
 def max_box(shape1, shape2):
@@ -309,34 +126,39 @@ def roi(img, is_threshold=False):
     cover_upper_right(img, is_threshold)
     return img
 
-def draw_bacteria(img, bact: Bacteria, color=[0, 255, 0]):
-    for x, y in bact.coords:
-        if bact.is_boundary(x, y):
-            for i, j in [[1,0], [0,1], [1,1], [1,-1]]:
-                for w in [1, -1]:
-                    xx = x + w * i
-                    yy = y + w * j
-                    if xx < 0 or yy < 0 or xx >= len(img) or yy >= len(img[0]):
-                        continue
-                    if [xx, yy] not in bact.coords:
-                        img[xx][yy] = color
-    # img[tuple(bact.get_center())] = [255, 255, 255]
-    # cv2.putText(img, str(abs(int(bact.img_mean(bact.img) - bact.img_mean(bact.bg_img())))), bact.get_center()[::-1], cv2.FONT_HERSHEY_SIMPLEX, 0.3, [0,0,0],1)
+def draw_bacteria(img, bact: Bacteria, color=[0, 255, 0], mode="draw"):
+    if mode == "draw":
+        for x, y in bact.coords:
+            if bact.is_boundary(x, y):
+                for i, j in [[1,0], [0,1], [1,1], [1,-1]]:
+                    for w in [1, -1]:
+                        xx = x + w * i
+                        yy = y + w * j
+                        if xx < 0 or yy < 0 or xx >= len(img) or yy >= len(img[0]):
+                            continue
+                        if [xx, yy] not in bact.coords:
+                            img[xx][yy] = color
+    elif mode == "rectangle":
+        gap = 2
+        cv2.rectangle(img, (max(bact.bounds.left - gap, 0), max(bact.bounds.top - gap, 0)), (min(bact.bounds.right + 2, len(img[0])), min(bact.bounds.bottom + 2, len(img))), color, 1)
+
     return img
 
 def invert_img(img):
     new_img = np.ones((img.shape))
     new_img -= img
-    return new_img
+    return new_img.astype(np.uint8)
 
 # Input a grayscaled image only
 def bg_normalize_img(img):
-    filtered = cv2.adaptiveThreshold(img, 1, cv2.ADAPTIVE_THRESH_MEAN_C,
-                                          cv2.THRESH_BINARY, 15, 0)
+    filtered = invert_img(custom_adaptive_threshold(img, 21, 5))
     masked = cv2.bitwise_and(img, img, mask=filtered)
     filtered_mean = masked.sum() / filtered.sum()
 
     normalized = (img / filtered_mean).astype(np.float32)
+
+    # mode = np.argmax(np.bincount(img.flatten()))
+    # normalized = (img / mode).astype(np.float32)
 
     nmax = np.max(normalized)
     nmin = np.min(normalized)
@@ -347,58 +169,103 @@ def bg_normalize_img(img):
     final = (normalized - nmin) * ((new_max - new_min) / (nmax - nmin)) + new_min
     return final.astype(np.uint8)
 
-def preprocess(orig_img, cover_corners=True, bias = 3):
-    img = orig_img.copy()
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = bg_normalize_img(img=img)
-    # plt.hist(img.flatten(), bins=20)
-    # plt.show()
-    img = cv2.adaptiveThreshold(img, 1, cv2.ADAPTIVE_THRESH_MEAN_C,
-                                          cv2.THRESH_BINARY, 15, bias)
-    img = invert_img(img)
-    if cover_corners:
-        img = roi(img, is_threshold=True)
-    return img
-    
-def generate_bacts(img, label, debug=False, bias = 3, size = [10, 200], debug_path = "for_debug.bmp", image_name = "current image", cover_corners=True, threshold = 2.0, max_diameter = 15):
-    processed_img = preprocess(img, cover_corners=cover_corners, bias= bias)
-    if debug:
-        debug_img = img.copy()
-    bacts = find_all_bact(processed_img, size=size, image_name=image_name)
-    bact_count = 0
-    max_shape = (1, 1)
-    final_bacts = []
-    for bact in bacts:
-        bact.retrieve_pixels(img)
-        bact.get_partial_img(img)
-        bact.get_bg_mean()
-        bact.label = label
+class BacteriaGenerator:
+    def __init__(self, first_bias, 
+                 second_bias, size_bounds, filter_threshold, first_block_size, 
+                 second_block_size, max_diameter, debug, cover_corners):
+        self.first_bias = first_bias
+        self.second_bias = second_bias
+        self.size_bounds = size_bounds
+        self.filter_threshold = filter_threshold
+        self.first_block_size = first_block_size
+        self.second_block_size = second_block_size
+        self.max_diameter = max_diameter
+        self.debug = debug
+        self.cover_corners = cover_corners
 
-        diff = abs(bact.img_mean(bact.img) - bact.img_mean(bact.bg_img()))
-        if diff < threshold:
-            continue
-
-        if bact.est_diameter() >= max_diameter:
-            continue
-
-        if debug:
-            draw_bacteria(debug_img, bact, [0, 255, 0])
-            bact_count += 1
-
-        final_bacts.append(bact)
-        max_shape = max_box(max_shape, bact.img_shape())
-    if debug:
-        cv2.imwrite(debug_path, debug_img)
-    return final_bacts, max_shape
+    def preprocess(self, orig_img):
+        img = orig_img.copy()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # img = bg_normalize_img(img=img)
+        first_threshold = custom_adaptive_threshold(img, self.first_block_size, const = self.first_bias, mode="foreground")
         
-# Test background
+        img = first_threshold
+        # first_filtered = cv2.bitwise_and(img, img, mask=first_threshold)
+        # img = custom_adaptive_threshold(first_filtered, self.second_block_size, const=self.second_bias, mode = "foreground")
+        if self.cover_corners:
+            img = roi(img, is_threshold=True)
+        return img
+    
+    def preprocess_v2(self, orig_img):
+        img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(img,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        if self.cover_corners:
+            img = roi(img, is_threshold=True)
+        return thresh
 
-def test_background():
-    img = cv2.imread("-3.bmp")
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = bg_normalize_img(img)
-    print(img)
-    plt.imshow(img)
-    plt.show()
+    
+    def generate_bacts(self, img, label, image_name = "current_image.bmp", debug_path = "for_debug"):
+        if self.debug:
+            debug_img = img.copy()
+        processed_img = self.preprocess(img)
+        bacts = find_all_bact(processed_img, size=self.size_bounds, image_name=image_name)
+        bact_count = 0
+        max_shape = (1, 1)
+        final_bacts = []
+        for bact in bacts:
+            for _ in range(3):
+                bact.expand_bacteria()
+            bact.retrieve_pixels(img)
+            bact.get_partial_img(img)
+            bact.get_bg_mean()
+            bact.label = label
 
-# test_background()
+            # bact_gray_img = np.mean(bact.img, axis=2).flatten().astype(np.uint8)
+            # bg_gray_image = np.mean(bact.bg_img(), axis=2).flatten().astype(np.uint8)
+            # diff = abs(np.median(np.bincount(bact_gray_img)) - np.median(np.bincount(bg_gray_image)))
+            # if diff < self.filter_threshold:
+            #     continue
+
+            if bact.est_diameter() >= self.max_diameter:
+                continue
+
+            if self.debug:
+                draw_bacteria(debug_img, bact, [0, 255, 0], mode="draw")
+                bact_count += 1
+
+            final_bacts.append(bact)
+            max_shape = max_box(max_shape, bact.img_shape())
+        if self.debug:
+            cv2.imwrite(os.path.join(debug_path, image_name), debug_img)
+            cv2.imwrite(os.path.join(debug_path, 'threshold_' + image_name), processed_img * 255)
+        return final_bacts, max_shape
+    
+
+    def f_generate_bacts(self, img, label, preprocess_key, image_name = "current image", debug_path = "for_debug.bmp"):
+        if self.debug:
+            debug_img = img.copy()
+        processed_img = preprocess_key(img)
+        bacts = find_all_bact(processed_img, size=self.size_bounds, image_name=image_name)
+        bact_count = 0
+        max_shape = (1, 1)
+        final_bacts = []
+        for bact in bacts:
+            bact.expand_bacteria()
+            bact.retrieve_pixels(img)
+            bact.get_partial_img(img)
+            bact.get_bg_mean()
+            bact.label = label
+
+            if bact.est_diameter() >= self.max_diameter:
+                continue
+
+            if self.debug:
+                draw_bacteria(debug_img, bact, [0, 255, 0], mode="draw")
+                bact_count += 1
+
+            final_bacts.append(bact)
+            max_shape = max_box(max_shape, bact.img_shape())
+        if self.debug:
+            cv2.imwrite(debug_path, debug_img)
+        return final_bacts, max_shape
+    
--- generate_data.py
+++ generate_data.py
@@ -1,16 +1,11 @@
 from extract_feature import *
 import os
-
-BIAS = 5
-MAX_DIAMETER = 10
-THRESHOLD = 2.8
-SWAB_THRESH = THRESHOLD
-SIZE = [10, 100]
+from hyperparameters import *
 
 directories = ["extracted_data", "debug"]
 
+# returns a list of images
 def get_img(bact:Bacteria):
-    # return (bact.img / 255.0).astype(np.float32)
     return bact.bg_normalized()
 
 for dir in directories:
@@ -28,11 +23,8 @@ test_bacts = []
 train_bacts = []
 max_shape = (28, 28) # images must be at least 28x28
 test_split_index = 1
-
-def draw_bact_hist(bacts, enabled = False):
-    if enabled:
-        plt.hist(np.mean(get_img(bacts[np.random.randint(0, len(bacts))]), axis=2).flatten())
-        plt.show()
+bacteria_generator = BacteriaGenerator(FIRST_BIAS, SECOND_BIAS, SIZE, THRESHOLD, FIRST_BLOCK_SIZE,
+                                       SECOND_BLOCK_SIZE, MAX_DIAMETER, True, True)
 
 if __name__ == "__main__":
     # load ecoli data
@@ -40,96 +32,102 @@ if __name__ == "__main__":
         name = "E.coli + {}.bmp".format(i)
         input_img_path = raw_folder + ecoli + name
         img = cv2.imread(input_img_path)
-        bacts, shape = generate_bacts(img, 0, bias = BIAS, size = SIZE, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = THRESHOLD, max_diameter=MAX_DIAMETER)
+        bacts, shape = bacteria_generator.generate_bacts(img, 
+                                                         0, 
+                                                         image_name = name, 
+                                                         debug_path = directories[1])
         if i <= test_split_index:
             test_bacts += bacts
         else:
             train_bacts += bacts
         max_shape = max_box(max_shape, shape)
-        draw_bact_hist(bacts)
 
     # load styphi data
     for i in range(1, 17):
         name = "S.typhi + {}.bmp".format(i)
         input_img_path = raw_folder + styphi + name
         img = cv2.imread(input_img_path)
-        bacts, shape = generate_bacts(img, 0, bias = BIAS, size = SIZE, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = THRESHOLD,  max_diameter=MAX_DIAMETER)
+        bacts, shape = bacteria_generator.generate_bacts(img, 
+                                                         0, 
+                                                         debug_path = directories[1], 
+                                                         image_name=name)
         if i <= test_split_index:
             test_bacts += bacts
         else:
             train_bacts += bacts
         max_shape = max_box(max_shape, shape)
-        draw_bact_hist(bacts)
 
+    bacteria_generator.cover_corners = False
     # load negative data
     for i in range(1, 12):
         name = "swab-{}.bmp".format(i)
         input_img_path = raw_folder + negative + "swab-{}.bmp".format(i)
         img = cv2.imread(input_img_path)
-        if i > 1:
-            bacts, shape = generate_bacts(img, 1, bias = BIAS, size = SIZE, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = SWAB_THRESH, max_diameter=MAX_DIAMETER)
-        else:
-            bacts, shape = generate_bacts(img, 1, bias = BIAS, size = SIZE, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = SWAB_THRESH, max_diameter=MAX_DIAMETER)
+        bacts, shape = bacteria_generator.generate_bacts(img, 
+                                                         1, 
+                                                         image_name = name, 
+                                                         debug_path = directories[1])
 
         if i <= test_split_index:
             test_bacts += bacts
         else:
             train_bacts += bacts
         max_shape = max_box(max_shape, shape)
-        draw_bact_hist(bacts)
 
     # load new negative data part 1
     for i in range(1, 5):
         name = "-{}.bmp".format(i)
         input_img_path = raw_folder + new_neg + "-{}.bmp".format(i)
         img = cv2.imread(input_img_path)
-        if i > 1:
-            bacts, shape = generate_bacts(img, 1, bias = BIAS, size = SIZE, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = SWAB_THRESH, max_diameter=MAX_DIAMETER)
-        else:
-            bacts, shape = generate_bacts(img, 1, bias = BIAS, size = SIZE, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = SWAB_THRESH, max_diameter=MAX_DIAMETER)
-
+        bacts, shape = bacteria_generator.generate_bacts(img, 
+                                                         1, 
+                                                         image_name = name, 
+                                                         debug_path = directories[1])
         if i <= test_split_index:
             test_bacts += bacts
         else:
             train_bacts += bacts
         max_shape = max_box(max_shape, shape)
-        draw_bact_hist(bacts)
 
     # load new negative data part 2
     for i in range(1, 5):
         name = "-small swab5-{}.bmp".format(i)
         input_img_path = raw_folder + new_neg + "-small swab5-{}.bmp".format(i)
         img = cv2.imread(input_img_path)
-        if i > 1:
-            bacts, shape = generate_bacts(img, 1, bias = BIAS, size = SIZE, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = SWAB_THRESH, max_diameter=MAX_DIAMETER)
-        else:
-            bacts, shape = generate_bacts(img, 1, bias = BIAS, size = SIZE, cover_corners= False, debug = True, debug_path = directories[1] + '/' + name, image_name=name, threshold = SWAB_THRESH, max_diameter=MAX_DIAMETER)
-
+        bacts, shape = bacteria_generator.generate_bacts(img, 
+                                                         1, 
+                                                         image_name = name, 
+                                                         debug_path = directories[1])
         if i <= test_split_index:
             test_bacts += bacts
         else:
             train_bacts += bacts
         max_shape = max_box(max_shape, shape)
-        draw_bact_hist(bacts)
 
 
     X = []
     y = []
-    for bact in train_bacts:
+    pbar = tqdm(train_bacts, total=len(train_bacts))
+    for bact in pbar:
+        pbar.set_description('saving training data')
         bact.pad_img(max_shape)
-        X.append(get_img(bact))
+        bact_imgs = get_img(bact)
+        X.append(bact_imgs)
         y.append(bact.label)
 
-    np.save(directories[0] + "/trainX.npy", X)
+    np.save(directories[0] + "/trainX.npy", np.array(X, dtype=object))
     np.save(directories[0] + "/trainY.npy", y)
 
     testX = []
     testY = []
-
-    for bact in test_bacts:
+    
+    pbar = tqdm(test_bacts, total = len(test_bacts))
+    for bact in pbar:
+        pbar.set_description('saving validation data')
         bact.pad_img(max_shape)
-        testX.append(get_img(bact))
+        img = get_img(bact)
+        testX.append(img)
         testY.append(bact.label)
 
-    np.save(directories[0] + "/testX.npy", testX)
+    np.save(directories[0] + "/testX.npy", np.array(testX, dtype=object))
     np.save(directories[0] + "/testY.npy", testY)
